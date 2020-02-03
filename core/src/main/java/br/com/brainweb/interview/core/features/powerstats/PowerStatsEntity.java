63a38ed3057e1970960b5d755ef763ac37b80104
Bruno Henrique de Castro <brunohcastro@hotmail.com>
2020-02-03 01:50:07 -0300

    feat(power-stats): add power stats feature
    - Enable caching and JDBCRepositories
    - Fix qualifier for transactionManager
    - Add PowerStats Repository and Service classes
    - Enable all endpoints for spring actuator

---
+++ core/src/main/java/br/com/brainweb/interview/core/features/powerstats/PowerStatsEntity.java
@@ -0,0 +1,34 @@
+package br.com.brainweb.interview.core.features.powerstats;
+
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Table("power_stats")
+public class PowerStatsEntity {
+
+    @Id
+    @With
+    @EqualsAndHashCode.Include
+    protected UUID id;
+
+    private Integer strength;
+    private Integer agility;
+    private Integer dexterity;
+    private Integer intelligence;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
